06f76d804ebcca2474204bdd818a20c3d7a67d1f
marina <marinamarrina@hotmail.com>
2021-12-01 12:13:29 +0100

    primi passi attualizzazione

--- admin/index.php
+++ admin/index.php
@@ -51,7 +51,7 @@
                     <td>$ <?php echo $propiedad['precio']; ?></td>
                     <td>
                         <a href="#" class="boton-rojo-block">Eliminar</a>
-                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
                 <?php endwhile; ?>
