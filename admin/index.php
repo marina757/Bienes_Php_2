c27aa8172ba086d58bd510bf4055fbe6919fa4dd
marina <marinamarrina@hotmail.com>
2021-12-01 10:35:43 +0100

    creando table per elencare prop del db

--- admin/index.php
+++ admin/index.php
@@ -19,6 +19,31 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="/imagenes/097331d0a6726ecbe77f0520b0a92726.jpg" class="imagen-tabla"> </td>
+                    <td>$120000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
