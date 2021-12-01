6937c47305567845591e64ce5d89bdb453aeb810
marina <marinamarrina@hotmail.com>
2021-12-01 11:08:30 +0100

    elencando prop

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,24 @@
 <?php
-
     // echo "<pre>";
     // var_dump($_GET);
     // echo "</pre>";
     // exit;
 
+    //IMPORTAR LA CONEXION
+    require '../includes/config/database.php';
+    $db = conectarDB(); 
+
+    //ESCRIBIR QUERY
+    $query = "SELECT * FROM propiedades";
+
+    //CONSULTAR DB
+    $resultadoConsulta = mysqli_query( $db, $query);
+
+    //MUESTRA MENSAJE CONDICIONAL
     $resultado = $_GET['resultado'] ?? null; //superglobal $_GET para valores que estan en url
     // var_dump($resultado);
 
+    //INCLUYE UN TEMPLATE
     require '../includes/funciones.php'; //sirve para funciones    
     incluirTemplate('header'); 
 ?>
@@ -31,21 +42,27 @@
                 </tr>
             </thead>
 
-            <tbody>
+            <tbody><!--MOSTRAR LOS RESULTADOS-->
+                <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>                
                 <tr>
-                    <td>1</td>
-                    <td>Casa en la playa</td>
-                    <td> <img src="/imagenes/097331d0a6726ecbe77f0520b0a92726.jpg" class="imagen-tabla"> </td>
-                    <td>$120000</td>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"> </td>
+                    <td>$ <?php echo $propiedad['precio']; ?></td>
                     <td>
                         <a href="#" class="boton-rojo-block">Eliminar</a>
                         <a href="#" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
+                <?php endwhile; ?>
             </tbody>
         </table>
     </main>
 
 <?php
+
+    //CERRAR LA CONEXION(OPCIONAL)
+    mysqli_close($db);
+
     incluirTemplate('footer');  
 ?>    
