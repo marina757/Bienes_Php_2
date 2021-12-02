474082e1fb1b579924add65c78160a6a8c6775a8
marina <marinamarrina@hotmail.com>
2021-12-02 15:57:11 +0100

    validation al form

--- login.php
+++ login.php
@@ -1,4 +1,33 @@
 <?php
+
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    //AUTENTICAR EL USUARIO
+
+    $errores = [];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $email = mysqli_real_escape_string($db, filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL) );
+        // var_dump($email);
+        $password = mysqli_real_escape_string($db, $_POST['password'] );
+
+        if(!$email) {
+            $errores[] = "El email es obligatorio o no es valido";
+        }
+
+        if(!$password) {
+            $errores[] = "El password es obligatorio";
+        }  
+        
+    }
+
+
+    //INCLUYE EL HEADER
     require 'includes/funciones.php'; //sirve para funciones    
     incluirTemplate('header'); 
 ?>
@@ -6,15 +35,21 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesion</h1>
 
-        <form class="formulario">
+        <?php foreach ($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
+
+        <form method="POST" class="formulario" novalidate>
             <fieldset><!--fieldset se pone cuando se agrupan campos relacionados-->
                 <legend>Email y Password</legend><!--legend explica lo que es el fieldset-->
 
                 <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu Email" id="email">
+                <input type="email" name="email" placeholder="Tu Email" id="email">
 
                 <label for="password">Password</label>
-                <input type="password" placeholder="Tu Password" id="password">
+                <input type="password" name="password" placeholder="Tu Password" id="password">
             </fieldset>
 
             <input type="submit" value="Iniciar Sesion" class="boton boton-verde">
